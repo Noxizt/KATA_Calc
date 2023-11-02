8efeec8451bdcda8c9f88431f2d6a9df7bdcf9df
Noxizt <97018839+Noxizt@users.noreply.github.com>
2023-11-03 00:38:26 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,12 +9,12 @@ public class Main {
     }
 
     public static String calc(String input) throws Exception {
-        String[] expressionArray = input.split(" ");           // Split
+        String[] expressionArray = input.split(" ");                               // Split
         if (expressionArray.length != 3) {
            throw new Exception("Ошибка ввода: Формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, *, /.");
         }
 
-        boolean checkArabic = true;                                  // Init..
+        boolean checkArabic = true;                                                // Init..
         boolean checkRomanNum1 = false;
         boolean checkRomanNum2 = false;
         int num1 = 0;
@@ -22,13 +22,13 @@ public class Main {
         int value = 0;
         String romanValue = "";
 
-        try {                                                        // Check Arabic [DONE]
+        try {                                                                      // Check Arabic [DONE]
             num1 = Integer.parseInt(expressionArray[0]);
             num2 = Integer.parseInt(expressionArray[2]);
         } catch (NumberFormatException e) {
             checkArabic = false;
             //System.out.println("checkArabic = " + checkArabic);
-            for (Numerals n : Numerals.values()) {                       // Check Roman [IN PROGRESS]
+            for (Numerals n : Numerals.values()) {                                 // Check Roman [IN PROGRESS]
                 if (expressionArray[0].equalsIgnoreCase(n.getRoman())) {
                     checkRomanNum1 = true;
                     num1 = n.getArabic();                     // num1 to arabic
@@ -38,7 +38,7 @@ public class Main {
                     num2 = n.getArabic();                 // num2 to arabic
                 }
             }
-            if (checkRomanNum1 && checkRomanNum2) {                     // Check both nums Roman
+            if (checkRomanNum1 && checkRomanNum2) {                                // Check both nums Roman
                 checkArabic = true;
             } else {
                 throw new Exception("Ошибка ввода: Используются одновременно разные системы счисления.");
@@ -46,10 +46,10 @@ public class Main {
         }
 
         if (checkArabic) {
-            if (num1 < 1 || num1 > 10 || num2 <1 || num2 > 10) {         // Check nums [DONE]
+            if (num1 < 1 || num1 > 10 || num2 <1 || num2 > 10) {                   // Check nums [DONE]
                 throw new Exception("Ошибка ввода: Допустимы целочисленные значения от 1 до 10 (I - X).");
             }
-            String arithmeticSign = expressionArray[1];               // Arithmetic [DONE]
+            String arithmeticSign = expressionArray[1];                            // Arithmetic [DONE]
             switch (arithmeticSign) {
                 case "+" -> value = num1 + num2;
                 case "-" -> value = num1 - num2;
@@ -61,7 +61,7 @@ public class Main {
             }
         }
 
-        if (checkRomanNum1 && checkRomanNum2) {                       // Arabic value to Roman
+        if (checkRomanNum1 && checkRomanNum2) {                                     // Arabic value to Roman
             if (value <1) {
                 throw new Exception("Ошибка вывода: В римской системе нет отрицательных чисел.");  //throw negative Roman
             }
@@ -77,7 +77,7 @@ public class Main {
             }
         }
 
-        if (checkRomanNum1 && checkRomanNum2) {                                  // for Output
+        if (checkRomanNum1 && checkRomanNum2) {                                     // for Output
             return romanValue;
         } else {
             String arabicValue = String.valueOf(value);
